d8ada2dadce1f823757113ab85b3f61c9523ffe3
melivaro <turbo3.6@cn.ru>
2019-10-17 22:16:45 +0700

    подключил таймер

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prime.heisen.me
+Limitations:
+    if your number N exceeds the execution time of the function, you will receive a time-out notification.
--- js/functionN.js
+++ js/functionN.js
@@ -1,20 +1,21 @@
 function getAllFactorsFor(numb) {
    
     let factors = [];
-    var integer = parseInt(numb);
-
-    setTimeout(stop_interval, 1000);
-    function stop_interval()
-    {
-      clearInterval(getAllFactorsFor);
-    }
+    let integer = parseInt(numb);
+    let timeIn = new Date().getSeconds();
 
     for (let i = 2; i <= integer; i++) {
+        let timeOut = new Date().getSeconds();
+        if (timeOut < timeIn + 5){
         while ((integer % i) === 0) {
             factors.push(i);
             integer /= i;
         }
     }
+    else {
+        return document.getElementById("output").innerHTML = "Time - out!"
+    }
+    }
    
-    return document.getElementById("output").innerHTML = factors;
+    return document.getElementById("output").innerHTML = "N = " + factors;
 }
